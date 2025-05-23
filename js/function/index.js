d3d0d49a3d58c19c32fc302dd1e318f1453e4b9d
“khoidang2110” <khoidang2110@gmail.com>
2025-05-23 14:55:12 +0700

    admin btn

--- js/function/index.js
+++ js/function/index.js
@@ -5,12 +5,53 @@ $(document).ready(function () {
 
   // Nếu có token, hiển thị nút Logout và ẩn nút Login
   if (token) {
+
+
+
+
+
       $('#login-btn').addClass('d-none');  // Ẩn nút Login
       $('#logout-btn').removeClass('d-none');  // Hiển thị nút Logout
       $("#user-menu").removeClass("d-none");
       $("#welcome-text")
         .text(`Welcome, ${fullName}`)
         .removeClass("d-none");
+
+
+
+   $.ajax({
+          url: 'https://java.thepointsaver.com/user/info',
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          success: function (res) {
+            const roleId = res?.data?.role?.id;
+          
+            if (roleId === 2 || roleId === 3) {
+              // Nếu role là 2 (staff) hoặc 3 (admin) thì hiển thị tên người dùng
+                  $('#admin-btn').removeClass('d-none');  // Hiển thị nút admin
+
+            } else {
+              // Nếu không phải admin hoặc manager thì ẩn
+      $('#admin-btn').addClass('d-none');  // Ẩn nút Login
+            }
+          },
+          error: function (xhr) {
+              if (xhr.status === 403) {
+                  // Nếu lỗi token (token sai, hết hạn...)
+                  alert('Phiên đăng nhập của bạn đã hết hạn. Vui lòng đăng nhập lại.');
+                  localStorage.removeItem('token'); // Xoá token đã lưu
+                  window.location.href = '../login.html'; // Điều hướng về trang đăng nhập
+              } else {
+                  // Nếu lỗi khác (lỗi mạng chẳng hạn) cũng điều hướng về trang chủ
+                  window.location.href = '../index.html';
+              }
+          }
+        });
+
+
+
   } else {
       $('#login-btn').removeClass('d-none');  // Hiển thị nút Login
       $('#logout-btn').addClass('d-none');  // Ẩn nút Logout
@@ -31,4 +72,7 @@ $(document).ready(function () {
       // Điều hướng đến trang login hoặc trang chủ (tuỳ chọn)
       window.location.href = '/login.html';  // Hoặc điều hướng về trang chủ: window.location.href = '/';
   });
+
+
+
 });
